7ce1b65527d12a01afb78d535297c08aa62ddc4a
Gergő Gelóczi <geloczigeri@gmail.com>
2021-12-19 19:10:32 +0100

    TranscodeMode enum descriptions

--- SynologyDotNet.AudioStation/TranscodeMode.cs
+++ SynologyDotNet.AudioStation/TranscodeMode.cs
@@ -1,4 +1,6 @@
-﻿namespace SynologyDotNet.AudioStation
+﻿using System.ComponentModel;
+
+namespace SynologyDotNet.AudioStation
 {
     /// <summary>
     /// Transcode mode
@@ -8,31 +10,37 @@
         /// <summary>
         /// No transcoding, the original data will be returned by the server.
         /// </summary>
+        [Description("None")]
         None,
 
         /// <summary>
         /// Transcode to MP3 320Kbps
         /// </summary>
+        [Description("MP3 320Kbps")]
         MP3_320Kbps,
 
         /// <summary>
         /// Transcode to MP3 256Kbps
         /// </summary>
+        [Description("MP3 256Kbps")]
         MP3_256Kbps,
 
         /// <summary>
         /// Transcode to MP3 192Kbps
         /// </summary>
+        [Description("MP3 192Kbps")]
         MP3_192Kbps,
 
         /// <summary>
         /// Transcode to MP3 128Kbps
         /// </summary>
+        [Description("MP3 128Kbps")]
         MP3_128Kbps,
 
         /// <summary>
         /// Transcode to WAV
         /// </summary>
+        [Description("WAV")]
         WAV
     }
 }
